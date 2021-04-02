43f8d3d045ceabe5a5571c34c71710c9ca7a9357
John Wonson <jmpwonson@gmail.com>
2021-04-02 15:31:00 +1100

    Revert "Reverting back repo"
    This reverts commit a2bfc3cb8d91a21d2119ef2ce7590c1b7c935030.

---
+++ cen207_unityproject/Assets/LoadingScene/SceneUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneUI : MonoBehaviour
+{
+    private void Awake()
+    {
+        transform.Find("backButton").GetComponent<Button_UI>().ClickFunc = () =>
+        {
+            Debug.Log("Click Back Button");
+            Loader.Load(Loader.Scene.MainMenu);
+        };
+    }
+}
---
+++ cen207_unityproject/Assets/LoadingScene/StartMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour
+{
+    private void Awake()
+    {
+        transform.Find("startBtn").GetComponent<Button_UI>().ClickFunc = () =>
+        {
+            Debug.Log("Click Start");
+            Loader.Load(Loader.Scene.GameScene);
+        };
+    }
+}
